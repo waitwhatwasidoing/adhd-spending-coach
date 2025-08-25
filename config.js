03cd836d4ff3814c5c3b5924c03177c849eb5186
waitwhatwasidoing <hello@waitwhatwasidoing.com>
2025-08-25 10:56:48 +0530

    config.js added

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  PURCHASE_URL: "https://buy.stripe.com/8wM8yQ3yQ8gK2yQeUU",
+  PLANNER_BASIC_URL: "https://buy.stripe.com/test_basic",
+  PLANNER_PREMIUM_URL: "https://buy.stripe.com/test_premium",
+};
